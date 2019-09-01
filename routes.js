0bf6573b0750fc12d7492b29d815437f4f0c7d74
Christopher Iliyas <christopher.iliyas@andela.com>
2019-09-01 12:44:08 +0100

    add mailing service

--- routes.js
+++ routes.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const webpush = require('web-push');
 const dotenv = require('dotenv');
+const Mailgun = require('mailgun-js')
 
 const router = new express.Router();
 dotenv.config();
 
-const { PUBLIC_VAPID_KEY, PRIVATE_VAPID_KEY} = process.env;
+const { PUBLIC_VAPID_KEY, PRIVATE_VAPID_KEY, MAILGUN_API_KEY, MAILGUN_DOMAIN } = process.env;
+const mailer = new Mailgun({ apiKey: MAILGUN_API_KEY, domain: MAILGUN_DOMAIN });
 webpush.setVapidDetails('mailto:test@email.org', PUBLIC_VAPID_KEY, PRIVATE_VAPID_KEY);
 
 router.post('/notify', (req, res) => {
     const subscription = req.body;
     res.status(201).json({});
     webpush.sendNotification(subscription, 'Test Notification');
+});
+
+router.get('/mail', (req, res) => {
+    req.body = `test mail y'all`;
+    const data = {
+        from: 'Drizzy <sender address>',
+        to: 'recipient-address',
+        subject: req.body,
+        text: 'Testing some Mailgun awesomeness!'
+      };
+
+    mailer.messages().send(data, (error, body) => {
+      if (error) return res.status(400).json({ message: error });
+      console.log(body);
+      return res.status(200).json({ message: 'sent' });
+    });
 });
 
 module.exports = router;
